f6c71a6ebe5557619198d1e3f6f4fa8f34bc23b8
Martin <mmpelloni@hotmail.com>
2020-08-21 02:33:40 -0300

    Agregando el DatasetBuilder

--- CVNet_DatasetBuilder/CVNet_DatasetBuilder.cpp
+++ CVNet_DatasetBuilder/CVNet_DatasetBuilder.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include "pch.h"
+#include "cmdlineopt.h"
+#include "Trainer.h"
+#include "CVNet.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n"; std::cout << "Hello World!\n"; std::cout << "Hello World!\n"; std::cout << "Hello World!\n";
-}
+
+int main(int argc, const char* argv[]) {
+    try {
+        // Opciones de línea de comando
+        // --path=C:\Repositories\CementCrack\Prueba --prefix=64x64 --size=64 --verbose 
+        CmdLineOpt::CmdLineOpt(argc, argv);
+
+        Trainer TRAINER;
+
+        return 0;
+    }
+    catch (std::exception e) {
+        std::cerr << "Error: " << e.what();
+    }
+};
---
+++ CVNet_DatasetBuilder/pch.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <stdlib.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#include <torch/torch.h>
+#include <torch/script.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#define OS_WINDOWS (defined(_WIN32) || defined(_WIN64)...)
+#ifdef OS_WINDOWS
+	#include <windows.h>
+#endif
+
+
+#include "cxxopts.hpp"
