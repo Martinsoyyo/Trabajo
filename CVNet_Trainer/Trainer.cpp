a0b24988e7562542cd567bf74e2342317433bf22
Martin <mmpelloni@hotmail.com>
2020-08-29 18:46:55 -0300

    sincronizando

--- CVNet_Trainer/Trainer.cpp
+++ CVNet_Trainer/Trainer.cpp
@@ -47,7 +47,7 @@ Trainer::Trainer():
 	// torch::optim::Adam optimizer(NET->parameters(), torch::optim::AdamOptions(1e-5).weight_decay(0.005).beta1(0.9).beta2(0.999));
     // Según la versión de Pytorch utilizada. Habría que ver cómo hacerlo en tiempo de compilación
 	//torch::optim::Adam optimizer(NET->parameters(), torch::optim::AdamOptions(2e-4).beta1(0.5));
-	torch::optim::Adam optimizer(NET->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple (0.5, 0.5)));	
+	torch::optim::Adam optimizer(NET->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple (0.9, 0.995)));	
 	
 	std::cout << NET << std::endl;
 
